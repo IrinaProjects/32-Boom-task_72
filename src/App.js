4a72c51157bbe9a4a2e8354a51207efe4b405ebd
Irina Spasova <irina.spasova.81@gmail.com>
2023-02-11 18:00:52 +0200

    Ready?

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Tags from "./components/Tags";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section class="hero">
+        <div class="hero-body">
+          <p class="title">A React Task</p>
+          <p class="subtitle">by Boom.dev</p>
+        </div>
+      </section>
+      <div class="container is-fullhd">
+        <div class="notification">
+          Edit the <code>./src</code> folder to add components.
+        </div>
+        <Tags tags={["boomdotdev", "task", "tags", "react"]} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
